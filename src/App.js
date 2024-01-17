766157966516f3debc4c54181a7a6cddb9658ac2
Thomas Loughran <tloughran3@gmail.com>
2024-01-17 16:41:17 +0000

    displays map with two markers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import { Icon } from 'leaflet';
 import './App.css';
+import "leaflet/dist/leaflet.css"
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import "./location-pin.png"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const markers = [
+        {
+            geocode: [52.489471, -1.898575],
+            popUp: "Start"
+        },
+        {
+            geocode: [52.489671, -1.899585],
+            popUp: "here"
+        }
+    ]
+
+    const customIcon = new Icon({
+        iconUrl : require("./location-pin.png"),
+        iconSize : [38, 38]
+    })
+
+    return (
+        <MapContainer center={[52.489471, -1.898575]} zoom={13}>
+            <TileLayer 
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {markers.map(marker => (
+                <Marker position={marker.geocode} icon={customIcon}></Marker>
+            ))}
+        </MapContainer>
+    );
 }
 
 export default App;

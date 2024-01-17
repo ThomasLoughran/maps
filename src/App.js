0776e89b28eba215f894e34c4f4fe345c100dae2
Thomas Loughran <tloughran3@gmail.com>
2024-01-17 17:32:49 +0000

    routes can be set across the map

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
-import { Icon } from 'leaflet';
-import './App.css';
-import "leaflet/dist/leaflet.css"
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
-import "./location-pin.png"
+import React from 'react';
+import Map from './Map';
+import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
 
-function App() {
-
-    const markers = [
-        {
-            geocode: [52.489471, -1.898575],
-            popUp: "Start"
-        },
-        {
-            geocode: [52.489671, -1.899585],
-            popUp: "here"
-        }
-    ]
-
-    const customIcon = new Icon({
-        iconUrl : require("./location-pin.png"),
-        iconSize : [38, 38]
-    })
-
-    return (
-        <MapContainer center={[52.489471, -1.898575]} zoom={13}>
-            <TileLayer 
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {markers.map(marker => (
-                <Marker position={marker.geocode} icon={customIcon}></Marker>
-            ))}
-        </MapContainer>
-    );
+export default function App() {
+  return (
+    <Map/>
+  );
 }
-
-export default App;
---
+++ src/Map.jsx
@@ -0,0 +1,54 @@
+
+// DO NOT GET RID OF
+import "leaflet/dist/leaflet.css"
+import React, { useEffect, useState, useRef } from "react";
+import L from "leaflet";
+import {
+  TileLayer,
+  MapContainer,
+  LayersControl
+} from "react-leaflet";
+
+import RoutingControl from './RoutingControl'
+
+const maps = {
+  base: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+};
+
+const Map = () => {
+  const [map, setMap] = useState(null);
+  const [start, setStart] = useState([38.9072, -77.0369])
+  const [end, setEnd] = useState([37.7749, -122.4194])
+
+  return (
+    <>
+      <MapContainer
+        center={[37.0902, -95.7129]}
+        zoom={3}
+        zoomControl={false}
+        style={{ height: "100vh", width: "100%", padding: 0 }}
+        whenCreated={map => setMap(map)}
+      >
+        {/* *************** */}
+        {/* Pass in our custom control layer here, inside of the map container */}
+        {/* *************** */}
+        <RoutingControl 
+          position={'topleft'} 
+          start={start} 
+          end={end} 
+          color={'#757de8'} 
+        />
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer checked name="Map">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url={maps.base}
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
+      </MapContainer>
+    </>
+  );
+};
+
+export default Map;
